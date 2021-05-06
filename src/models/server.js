a128b35524f0c2254fb78663b4a38fb91336059c
Mariano Huitron <mariano.14th@gmail.com>
2021-05-05 21:44:09 -0600

    add cors

--- src/models/server.js
+++ src/models/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const Sockets = require('./sockets');
+const cors = require('cors');
 require('dotenv').config();
 
 class Server {
@@ -11,6 +12,7 @@ class Server {
         
         this.port = process.env.PORT;
         this.app = express();
+        this.app.use(cors());
         this.server = http.createServer(this.app);
         this.io = socketio(this.server, {});
 
